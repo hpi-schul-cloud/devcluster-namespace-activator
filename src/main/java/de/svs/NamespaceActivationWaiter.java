210b9e6dc88f103f53c6aa3a7b72bd80c5e76018
Phillip Wirth <phillip.wirth@dataport.de>
2024-02-09 09:23:25 +0100

    made waiting tries configureable

--- src/main/java/de/svs/NamespaceActivationWaiter.java
+++ src/main/java/de/svs/NamespaceActivationWaiter.java
@@ -24,19 +24,25 @@ public class NamespaceActivationWaiter {
     @ConfigProperty(name = "baseDomain")
     String baseDomain;
 
+    @ConfigProperty(name = "waiter.tries", defaultValue = "30")
+    int tries;
+
+    @ConfigProperty(name = "waiter.delay", defaultValue = "2")
+    int delay;
+
     Multi<OutboundSseEvent> waitForNamespaceToBecomeAvailable(String namespace) {
         AtomicBoolean finalMessageReceived = new AtomicBoolean();
 
         return Multi.createBy()
                 .repeating()
                 .supplier(Unchecked.supplier(() -> new NamespaceStatus(objectMapper, baseDomain).get(namespace)))
-                .withDelay(Duration.ofSeconds(2))
+                .withDelay(Duration.ofSeconds(delay))
                 .until(outboundSseEvent -> finalMessageReceived.getAndSet(outboundSseEvent.finalMessage()))
                 .map(Unchecked.function(statusDto -> sse.newEventBuilder()
                         .name("namespace-status")
                         .data(String.class, objectMapper.writeValueAsString(statusDto))
                         .build()))
                 .select()
-                .first(10);
+                .first(tries);
     }
 }
